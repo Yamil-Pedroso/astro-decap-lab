25be81ace78fa2cf44a63eb3ce7d6eb0e88f1ed0
Yamil Pedroso <yamirovinci@gmail.com>
2026-09-18 09:51:39 +0200

    New topics in formular added

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,6 +2,10 @@ import type { ContactTopic } from "../types/contact";
 
 export const contactTopics: ContactTopic[] = [
   { label: "Product question", value: "product" },
+  { label: "PC Repair", value: "pc_repair" },
+  { label: "IT Support", value: "it_support" },
+  { label: "Web Development", value: "web_development" },
+  { label: "Consulting", value: "consulting" },
   { label: "Shipping", value: "shipping" },
   { label: "Warranty", value: "warranty" },
   { label: "Returns", value: "returns" },
